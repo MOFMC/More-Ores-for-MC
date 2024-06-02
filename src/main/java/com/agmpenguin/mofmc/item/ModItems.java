38df6fb82b0cf1570b8238a5789e6fa9468c735f
AGMPenguin <manleyaidan08@gmail.com>
2024-06-02 12:09:48 -0400

    Changes!

--- src/main/java/com/agmpenguin/mofmc/item/ModItems.java
+++ src/main/java/com/agmpenguin/mofmc/item/ModItems.java
@@ -14,24 +14,23 @@ public class ModItems {
 
     public static final RegistryObject<Item> RUBY = createModItem("ruby");
     public static final RegistryObject<Item> RAW_RUBY = createModItem("raw_ruby");
-    public static final RegistryObject<Item> RUBY_SWORD = createModSwordItem("ruby_sword", ModTiers.RUBY, 2, 3f);
-    public static final RegistryObject<Item> RUBY_PICKAXE = createModPickaxeItem("ruby_pickaxe", ModTiers.RUBY, 1, 1f);
-    public static final RegistryObject<Item> RUBY_AXE = createModAxeItem("ruby_axe", ModTiers.RUBY, 4, 0f);
-    public static final RegistryObject<Item> RUBY_SHOVEL = createModShovelItem("ruby_shovel", ModTiers.RUBY, 0, 1f);
+    public static final RegistryObject<Item> RUBY_SWORD = createModSwordItem("ruby_sword", ModTiers.RUBY, 3, -2.4f);
+    public static final RegistryObject<Item> RUBY_PICKAXE = createModPickaxeItem("ruby_pickaxe", ModTiers.RUBY, 1, -2.8f);
+    public static final RegistryObject<Item> RUBY_AXE = createModAxeItem("ruby_axe", ModTiers.RUBY, 6.0f, -3.0f);
+    public static final RegistryObject<Item> RUBY_SHOVEL = createModShovelItem("ruby_shovel", ModTiers.RUBY, 1.5f, -3.0f);
     public static final RegistryObject<Item> RUBY_HOE = createModHoeItem("ruby_hoe", ModTiers.RUBY, 0, 0f);
     public static final RegistryObject<Item> RUBY_HELMET = createModArmorItem("ruby_helmet", ModArmorMaterials.RUBY, ArmorItem.Type.HELMET);
     public static final RegistryObject<Item> RUBY_CHESTPLATE = createModArmorItem("ruby_chestplate", ModArmorMaterials.RUBY, ArmorItem.Type.CHESTPLATE);
     public static final RegistryObject<Item> RUBY_LEGGINGS = createModArmorItem("ruby_leggings", ModArmorMaterials.RUBY, ArmorItem.Type.LEGGINGS);
     public static final RegistryObject<Item> RUBY_BOOTS = createModArmorItem("ruby_boots", ModArmorMaterials.RUBY, ArmorItem.Type.BOOTS);
-    public static final RegistryObject<Item> RUBY_V_1 = createModItem("ruby_v_1");
 
     public static final RegistryObject<Item>
             PYRITE_INGOT = createModItem("pyrite_ingot"),
             RAW_PYRITE = createModItem("raw_pyrite"),
-            PYRITE_SWORD = createModSwordItem("pyrite_sword", ModTiers.PYRITE, 2, 3f),
-            PYRITE_PICKAXE = createModPickaxeItem("pyrite_pickaxe", ModTiers.PYRITE, 1, 1f),
-            PYRITE_AXE = createModAxeItem("pyrite_axe", ModTiers.PYRITE, 4, 0f),
-            PYRITE_SHOVEL = createModShovelItem("pyrite_shovel", ModTiers.PYRITE, 0, 1f),
+            PYRITE_SWORD = createModSwordItem("pyrite_sword", ModTiers.PYRITE, 3, -2.4f),
+            PYRITE_PICKAXE = createModPickaxeItem("pyrite_pickaxe", ModTiers.PYRITE, 1, -2.8f),
+            PYRITE_AXE = createModAxeItem("pyrite_axe", ModTiers.PYRITE, 3f, -3.0f),
+            PYRITE_SHOVEL = createModShovelItem("pyrite_shovel", ModTiers.PYRITE, 1.5f, -3.0f),
             PYRITE_HOE = createModHoeItem("pyrite_hoe", ModTiers.PYRITE, 0, 0f),
             PYRITE_HELMET = createModArmorItem("pyrite_helmet", ModArmorMaterials.PYRITE, ArmorItem.Type.HELMET),
             PYRITE_CHESTPLATE = createModArmorItem("pyrite_chestplate", ModArmorMaterials.PYRITE, ArmorItem.Type.CHESTPLATE),
@@ -40,10 +39,10 @@ public class ModItems {
 
     public static final RegistryObject<Item> TITANIUM_ROD = createModItem("titanium_rod");
     public static final RegistryObject<Item> TITANIUM_DUST = createModItem("titanium_dust");
-    public static final RegistryObject<Item> TITANIUM_SWORD = createModSwordItem("titanium_sword", ModTiers.TITANIUM, 2, 3f);
-    public static final RegistryObject<Item> TITANIUM_PICKAXE = createModPickaxeItem("titanium_pickaxe", ModTiers.TITANIUM, 1, 1f);
-    public static final RegistryObject<Item> TITANIUM_AXE = createModAxeItem("titanium_axe", ModTiers.TITANIUM, 4, 0f);
-    public static final RegistryObject<Item> TITANIUM_SHOVEL = createModShovelItem("titanium_shovel", ModTiers.TITANIUM, 0, 1f);
+    public static final RegistryObject<Item> TITANIUM_SWORD = createModSwordItem("titanium_sword", ModTiers.TITANIUM, 3, -2.4f);
+    public static final RegistryObject<Item> TITANIUM_PICKAXE = createModPickaxeItem("titanium_pickaxe", ModTiers.TITANIUM, 1, -2.8f);
+    public static final RegistryObject<Item> TITANIUM_AXE = createModAxeItem("titanium_axe", ModTiers.TITANIUM, 4f, -3.0f);
+    public static final RegistryObject<Item> TITANIUM_SHOVEL = createModShovelItem("titanium_shovel", ModTiers.TITANIUM, 1.5f, -3.0f);
     public static final RegistryObject<Item> TITANIUM_HOE = createModHoeItem("titanium_hoe", ModTiers.TITANIUM, 0, 0f);
     public static final RegistryObject<Item> TITANIUM_HELMET = createModArmorItem("titanium_helmet", ModArmorMaterials.TITANIUM, ArmorItem.Type.HELMET);
     public static final RegistryObject<Item> TITANIUM_CHESTPLATE = createModArmorItem("titanium_chestplate", ModArmorMaterials.TITANIUM, ArmorItem.Type.CHESTPLATE);
@@ -52,10 +51,10 @@ public class ModItems {
 
     public static final RegistryObject<Item> PURPLE_SAPPHIRE = createModItem("purple_sapphire");
     public static final RegistryObject<Item> RAW_PURPLE_SAPPHIRE = createModItem("raw_purple_sapphire");
-    public static final RegistryObject<Item> PURPLE_SAPPHIRE_SWORD = createModSwordItem("purple_sapphire_sword", ModTiers.PURPLE_SAPPHIRE, 2, 3f);
-    public static final RegistryObject<Item> PURPLE_SAPPHIRE_PICKAXE = createModPickaxeItem("purple_sapphire_pickaxe", ModTiers.PURPLE_SAPPHIRE, 1, 1f);
-    public static final RegistryObject<Item> PURPLE_SAPPHIRE_AXE = createModAxeItem("purple_sapphire_axe", ModTiers.PURPLE_SAPPHIRE, 4, 0f);
-    public static final RegistryObject<Item> PURPLE_SAPPHIRE_SHOVEL = createModShovelItem("purple_sapphire_shovel", ModTiers.PURPLE_SAPPHIRE, 0, 1f);
+    public static final RegistryObject<Item> PURPLE_SAPPHIRE_SWORD = createModSwordItem("purple_sapphire_sword", ModTiers.PURPLE_SAPPHIRE, 3, -2.4f);
+    public static final RegistryObject<Item> PURPLE_SAPPHIRE_PICKAXE = createModPickaxeItem("purple_sapphire_pickaxe", ModTiers.PURPLE_SAPPHIRE, 1, -2.8f);
+    public static final RegistryObject<Item> PURPLE_SAPPHIRE_AXE = createModAxeItem("purple_sapphire_axe", ModTiers.PURPLE_SAPPHIRE, 5f, -3.0f);
+    public static final RegistryObject<Item> PURPLE_SAPPHIRE_SHOVEL = createModShovelItem("purple_sapphire_shovel", ModTiers.PURPLE_SAPPHIRE, 1.5f, -3.0f);
     public static final RegistryObject<Item> PURPLE_SAPPHIRE_HOE = createModHoeItem("purple_sapphire_hoe", ModTiers.PURPLE_SAPPHIRE, 0, 0f);
     public static final RegistryObject<Item> PURPLE_SAPPHIRE_HELMET = createModArmorItem("purple_sapphire_helmet", ModArmorMaterials.PURPLE_SAPPHIRE, ArmorItem.Type.HELMET);
     public static final RegistryObject<Item> PURPLE_SAPPHIRE_CHESTPLATE = createModArmorItem("purple_sapphire_chestplate", ModArmorMaterials.PURPLE_SAPPHIRE, ArmorItem.Type.CHESTPLATE);
@@ -64,10 +63,10 @@ public class ModItems {
 
     public static final RegistryObject<Item> TIN_INGOT = createModItem("tin_ingot");
     public static final RegistryObject<Item> RAW_TIN = createModItem("raw_tin");
-    public static final RegistryObject<Item> TIN_SWORD = createModSwordItem("tin_sword", ModTiers.TIN, 2, 3f);
-    public static final RegistryObject<Item> TIN_PICKAXE = createModPickaxeItem("tin_pickaxe", ModTiers.TIN, 1, 1f);
-    public static final RegistryObject<Item> TIN_AXE = createModAxeItem("tin_axe", ModTiers.TIN, 4, 0f);
-    public static final RegistryObject<Item> TIN_SHOVEL = createModShovelItem("tin_shovel", ModTiers.TIN, 0, 1f);
+    public static final RegistryObject<Item> TIN_SWORD = createModSwordItem("tin_sword", ModTiers.TIN, 3, -2.4f);
+    public static final RegistryObject<Item> TIN_PICKAXE = createModPickaxeItem("tin_pickaxe", ModTiers.TIN, 1, -2.8f);
+    public static final RegistryObject<Item> TIN_AXE = createModAxeItem("tin_axe", ModTiers.TIN, 3f, -3.0f);
+    public static final RegistryObject<Item> TIN_SHOVEL = createModShovelItem("tin_shovel", ModTiers.TIN, 1.5f, -3.0f);
     public static final RegistryObject<Item> TIN_HOE = createModHoeItem("tin_hoe", ModTiers.TIN, 0, 0f);
     public static final RegistryObject<Item> TIN_HELMET = createModArmorItem("tin_helmet", ModArmorMaterials.TIN, ArmorItem.Type.HELMET);
     public static final RegistryObject<Item> TIN_CHESTPLATE = createModArmorItem("tin_chestplate", ModArmorMaterials.TIN, ArmorItem.Type.CHESTPLATE);
@@ -75,10 +74,10 @@ public class ModItems {
     public static final RegistryObject<Item> TIN_BOOTS = createModArmorItem("tin_boots", ModArmorMaterials.TIN, ArmorItem.Type.BOOTS);
 
     public static final RegistryObject<Item> RAINBOW_GEM = createModItem("rainbow_gem");
-    public static final RegistryObject<Item> RAINBOW_SWORD = createModSwordItem("rainbow_sword", ModTiers.RAINBOW, 10, 6f);
-    public static final RegistryObject<Item> RAINBOW_PICKAXE = createModPickaxeItem("rainbow_pickaxe", ModTiers.RAINBOW, 8, 4f);
-    public static final RegistryObject<Item> RAINBOW_AXE = createModAxeItem("rainbow_axe", ModTiers.RAINBOW, 14, 2f);
-    public static final RegistryObject<Item> RAINBOW_SHOVEL = createModShovelItem("rainbow_shovel", ModTiers.RAINBOW, 4, 3f);
+    public static final RegistryObject<Item> RAINBOW_SWORD = createModSwordItem("rainbow_sword", ModTiers.RAINBOW, 3, -2.4f);
+    public static final RegistryObject<Item> RAINBOW_PICKAXE = createModPickaxeItem("rainbow_pickaxe", ModTiers.RAINBOW, 1, -2.8f);
+    public static final RegistryObject<Item> RAINBOW_AXE = createModAxeItem("rainbow_axe", ModTiers.RAINBOW, 5.5f, -3.0f);
+    public static final RegistryObject<Item> RAINBOW_SHOVEL = createModShovelItem("rainbow_shovel", ModTiers.RAINBOW, 1.5f, -3.0f);
     public static final RegistryObject<Item> RAINBOW_HOE = createModHoeItem("rainbow_hoe", ModTiers.RAINBOW, 28, 10f);
     public static final RegistryObject<Item> RAINBOW_HELMET = createModArmorItem("rainbow_helmet", ModArmorMaterials.RAINBOW, ArmorItem.Type.HELMET);
     public static final RegistryObject<Item> RAINBOW_CHESTPLATE = createModArmorItem("rainbow_chestplate", ModArmorMaterials.RAINBOW, ArmorItem.Type.CHESTPLATE);
@@ -96,11 +95,11 @@ public class ModItems {
         return ITEMS.register(name, () -> new PickaxeItem(tier, attack, speed, new Item.Properties()));
     }
 
-    private static RegistryObject<Item> createModShovelItem(String name, ForgeTier tier, int attack, float speed) {
+    private static RegistryObject<Item> createModShovelItem(String name, ForgeTier tier, float attack, float speed) {
         return ITEMS.register(name, () -> new ShovelItem(tier, attack, speed, new Item.Properties()));
     }
 
-    private static RegistryObject<Item> createModAxeItem(String name, ForgeTier tier, int attack, float speed) {
+    private static RegistryObject<Item> createModAxeItem(String name, ForgeTier tier, float attack, float speed) {
         return ITEMS.register(name, () -> new AxeItem(tier, attack, speed, new Item.Properties()));
     }
 
--- src/main/java/com/agmpenguin/mofmc/item/ModTiers.java
+++ src/main/java/com/agmpenguin/mofmc/item/ModTiers.java
@@ -9,12 +9,12 @@ import net.minecraftforge.common.ForgeTier;
 import net.minecraftforge.registries.RegistryObject;
 
 public class ModTiers {
-    public static final ForgeTier RUBY = createForgeTier(2, 250, 6.0f, 2.0f, 14, BlockTags.NEEDS_IRON_TOOL, ModItems.RUBY);
-    public static final ForgeTier PYRITE = createForgeTier(2, 250, 6.0f, 2.0f, 7, BlockTags.NEEDS_IRON_TOOL, ModItems.PYRITE_INGOT);
-    public static final ForgeTier TITANIUM = createForgeTier(2, 420, 6.0f, 2.0f, 12, BlockTags.NEEDS_IRON_TOOL, ModItems.TITANIUM_ROD);
-    public static final ForgeTier PURPLE_SAPPHIRE = createForgeTier(2, 250, 6.0f, 2.0f, 14, BlockTags.NEEDS_IRON_TOOL, ModItems.PURPLE_SAPPHIRE);
-    public static final ForgeTier TIN = createForgeTier(2, 125, 6.0f, 2.0f, 6, BlockTags.NEEDS_STONE_TOOL, ModItems.TIN_INGOT);
-    public static final ForgeTier RAINBOW = createForgeTier(2, 500, 6.0f, 2.0f, 26, BlockTags.NEEDS_DIAMOND_TOOL, ModItems.RAINBOW_GEM);
+    public static final ForgeTier RUBY = createForgeTier(3, 1790, 9.0f, 3.5f, 9, BlockTags.NEEDS_IRON_TOOL, ModItems.RUBY);
+    public static final ForgeTier PYRITE = createForgeTier(1, 200, 5.0f, 1.0f, 7, BlockTags.NEEDS_IRON_TOOL, ModItems.PYRITE_INGOT);
+    public static final ForgeTier TITANIUM = createForgeTier(2, 420, 4.0f, 2.0f, 12, BlockTags.NEEDS_IRON_TOOL, ModItems.TITANIUM_ROD);
+    public static final ForgeTier PURPLE_SAPPHIRE = createForgeTier(3, 1323, 7.0f, 2.5f, 14, BlockTags.NEEDS_IRON_TOOL, ModItems.PURPLE_SAPPHIRE);
+    public static final ForgeTier TIN = createForgeTier(0, 50, 2.0f, 0.0f, 3, BlockTags.NEEDS_STONE_TOOL, ModItems.TIN_INGOT);
+    public static final ForgeTier RAINBOW = createForgeTier(3, 1474, 8.0f, 3.0f, 16, BlockTags.NEEDS_DIAMOND_TOOL, ModItems.RAINBOW_GEM);
 
     private static ForgeTier createForgeTier(int level, int uses, float speed, float attackDamageBonus, int enchantmentValue, TagKey<Block> toolTypeNeeded, RegistryObject<Item> Material) {
         return new ForgeTier(level, uses, speed, attackDamageBonus, enchantmentValue, toolTypeNeeded, () -> Ingredient.of(Material.get()));
