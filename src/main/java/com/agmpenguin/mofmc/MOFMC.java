a92a37a2737c2f32e7946ae44a8ac84cafeaa1f3
AGMPenguin <manleyaidan08@gmail.com>
2024-07-16 17:13:46 -0400

    d

--- src/main/java/com/agmpenguin/mofmc/MOFMC.java
+++ src/main/java/com/agmpenguin/mofmc/MOFMC.java
@@ -1,9 +1,17 @@
 package com.agmpenguin.mofmc;
 
+import com.agmpenguin.mofmc.block.ModBlocks;
+import com.agmpenguin.mofmc.item.ModItems;
+import com.agmpenguin.mofmc.item.ModTabs;
+import com.agmpenguin.mofmc.world.gen.ModOreGeneration;
 import net.fabricmc.api.ModInitializer;
 
+import net.minecraft.world.gen.YOffset;
+import net.minecraft.world.gen.feature.ConfiguredFeatures;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import static net.minecraft.world.gen.feature.OrePlacedFeatures.ORE_DIAMOND;
 
 public class MOFMC implements ModInitializer {
 	public static final String MOD_ID = "mofmc";
@@ -11,7 +19,10 @@ public class MOFMC implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
-		LOGGER.info("Hello Fabric world!");
+		ModTabs.registerModTabs();
+		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
+		ModOreGeneration.generateOres();
+		LOGGER.info("\nHello There!\nWhat are you doing here?\n...\nOh yeah - this is the log.\nDoes this show up on crash logs?");
 	}
 }
--- src/main/java/com/agmpenguin/mofmc/MOFMCDataGenerator.java
+++ src/main/java/com/agmpenguin/mofmc/MOFMCDataGenerator.java
@@ -1,11 +1,29 @@
 package com.agmpenguin.mofmc;
 
+import com.agmpenguin.mofmc.datagen.*;
+import com.agmpenguin.mofmc.world.ModConfiguredFeatures;
+import com.agmpenguin.mofmc.world.ModPlacedFeatures;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.minecraft.registry.RegistryBuilder;
+import net.minecraft.registry.RegistryKeys;
 
 public class MOFMCDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
+		pack.addProvider(ModBlockTagProvider::new);
+		pack.addProvider(ModItemTagProvider::new);
+		pack.addProvider(ModLootTableProvider::new);
+		pack.addProvider(ModModelProvider::new);
+		pack.addProvider(ModRecipeProvider::new);
+		pack.addProvider(ModWorldGenerator::new);
+	}
+
+	@Override
+	public void buildRegistry(RegistryBuilder registryBuilder) {
+		registryBuilder.addRegistry(RegistryKeys.CONFIGURED_FEATURE, ModConfiguredFeatures::bootstrap);
+		registryBuilder.addRegistry(RegistryKeys.PLACED_FEATURE, ModPlacedFeatures::bootstrap);
 	}
 }
